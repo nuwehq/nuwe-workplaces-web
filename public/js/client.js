f47f5b6e3869056f11a70c8c944fe1ec85ee5b81
Steve Schofield <stevebschofield@gmail.com>
2015-02-17 19:58:18 +0100

    fix for jade template

--- public/js/client.js
+++ public/js/client.js
@@ -1,40 +1,40 @@
 $(function(){
 
-  $.get('/teams', appendToList);
+  // $.get('/teams', appendToList);
 
-  $('form').on('submit', function(event) {
-    event.preventDefault();
+  // $('form').on('submit', function(event) {
+  //   event.preventDefault();
 
-    var form = $(this);
-    var teamData = form.serialize();
+  //   var form = $(this);
+  //   var teamData = form.serialize();
 
-    $('.alert').hide();
+  //   $('.alert').hide();
 
-    $.ajax({
-      type: 'POST', url: '/teams', data: teamData
-    })
-    .error(function() {
-      $('.alert').show();
-    })
-    .success(function(teamName){
-      appendToList([teamName]);
-      form.trigger('reset');
-    });
-  });
+  //   $.ajax({
+  //     type: 'POST', url: '/teams', data: teamData
+  //   })
+  //   .error(function() {
+  //     $('.alert').show();
+  //   })
+  //   .success(function(teamName){
+  //     appendToList([teamName]);
+  //     form.trigger('reset');
+  //   });
+  // });
 
-  function appendToList(teams) {
-    var list = [];
-    var content, team;
-    for(var i in teams){
-      team = teams[i];
-      content = '<a href="/teams/'+team+'">'+team+'</a>'+ // + // example on how to serve static images
-        ' <a href="#" data-team="'+team+'">'+
-        '<img src="/img/delete.png" width="15px"></a>';
-      list.push($('<li>', { html: content }));
-    }
+  // function appendToList(teams) {
+  //   var list = [];
+  //   var content, team;
+  //   for(var i in teams){
+  //     team = teams[i];
+  //     content = '<a href="/teams/'+team+'">'+team+'</a>'+ // + // example on how to serve static images
+  //       ' <a href="#" data-team="'+team+'">'+
+  //       '<img src="/img/delete.png" width="15px"></a>';
+  //     list.push($('<li>', { html: content }));
+  //   }
 
-    $('.team-list').append(list)
-  }
+  //   $('.team-list').append(list)
+  // }
 
 
   $('.team-list').on('click', 'a[data-team]', function (event) {
